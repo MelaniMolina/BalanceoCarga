a72d5eac532c093e97e3b18850581d4c1b21b2e9
MelaniMolina <melani.molina@epn.edu.ec>
2024-01-15 15:27:44 -0500

    first commit

---
+++ Servidor4/servidor4.py
@@ -0,0 +1,7 @@
+from flask import Flask
+servidor4 = Flask(__name__)
+@servidor4.route('/')
+def hola():
+    return"Hola desde el servidor4!!"
+if __name__=='__main__':
+    servidor4.run(host='0.0.0.0')
---
+++ Servidor5/servidor5.py
@@ -0,0 +1,7 @@
+from flask import Flask
+servidor5 = Flask(__name__)
+@servidor5.route('/')
+def hola():
+    return"Hola desde el servidor5!!"
+if __name__=='__main__':
+    servidor5.run(host='0.0.0.0')
